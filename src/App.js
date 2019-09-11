80ed54a167ed8753c1e722c683aeec7fab5a3b38
baze <wrrwrr111@gmail.com>
2019-09-11 17:03:55 +0800

    棋盘框架
    棋盘静态框架
    新增了选中棋子，旋转 反转

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ function App() {
   );
 }
 
-class SquareItem extends React.Component{
-  render(){
+class SquareItem extends React.Component {
+  render() {
     return <div className='cel'></div>
   }
 }
-class Board extends React.Component{
-  render(){
+class Board extends React.Component {
+  render() {
     const boardMatrix = this.props.boardMatrix
-    const squareItems = boardMatrix.valueOf().map((row,rowIndex)=>{
-      return row.map((col,colIndex)=>{
+    const squareItems = boardMatrix.valueOf().map((row, rowIndex) => {
+      return row.map((col, colIndex) => {
         return (
-          <SquareItem></SquareItem>
+          <SquareItem key={rowIndex*5+colIndex}></SquareItem>
         )
       })
     })
@@ -30,219 +30,317 @@ class Board extends React.Component{
     )
   }
 }
-class PieceItem extends React.Component{
-  render(){
-    const pieceData = this.props.pieceData;
-    const pieces = pieceData.map((row)=>{
-      return row.map(col=>{
-        return <div className={col?'cel blue':'cel'}></div>
+class PieceItem extends React.Component {
+  render() {
+    const piece = this.props.piece;
+    const isSelected = this.props.isSelected;
+    if (isSelected) {
+      //选中时 添加反转和旋转
+      const pieces = piece.pieceData.map((row, rowIndex) => {
+        return row.map((col, colIndex) => {
+          if (rowIndex === 0 && colIndex === 0) {
+            return <div className='cel' onClick={this.props.rotatePiece} key={rowIndex*5+colIndex}>转</div>
+          }
+          if (rowIndex === 0 && colIndex === 4) {
+            return <div className='cel' onClick={this.props.mirrorPiece} key={rowIndex*5+colIndex}>翻</div>
+          }
+          return <div className={col?`cel ${piece.pieceClass}`:'cel'} key={rowIndex*5+colIndex}></div>
+        })
       })
-    })
-    return <div class='piece'>{pieces}</div>
+      return <div className={`piece selected_${piece.playerIndex}`}>{pieces}</div>
+    } else {
+      const pieces = piece.pieceData.map((row, rowIndex) => {
+        return row.map((col, colIndex) => {
+          return <div className={col?`cel ${piece.pieceClass}`:'cel'} key={rowIndex*5+colIndex}></div>
+        })
+      })
+      return <div className='piece' onClick={this.props.selectPiece}>{pieces}</div>
+    }
   }
 }
-class PlayerItem extends React.Component{
-  render(){
-    const pieces = this.props.pieces;
-    const pieceItems = pieces.map((pieceData,pieceIndex)=>{
-      return(
-        <PieceItem pieceData={pieceData} key={pieceIndex}></PieceItem>
+class PlayerItem extends React.Component {
+  render() {
+    const player = this.props.player;
+    const pieceItems = player.pieces.map((piece, pieceIndex) => {
+      return (
+        <PieceItem 
+        piece={piece} 
+        selectPiece={(e)=>{this.props.selectPiece(piece,e)}} 
+        key={pieceIndex}>
+        </PieceItem>
       )
     })
 
     return (
-      <div className='player player1'>
+      <div className={`player player_${player.index}`}>
         {pieceItems}
       </div>
     )
   }
 }
 
-class Game extends React.Component{
-  constructor(props){
+class Game extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      boardMatrix:math.zeros(20,20),
-      players:[{
-        pieces:pieces,
-      }],
+      boardMatrix: math.zeros(20, 20),
+      players: ['red', 'yellow', 'blue', 'green'].map((color, index) => {
+        let id = Math.random().toFixed(5)*100000+1; // TODO 
+        return {
+          index,
+          id,
+          pieces: PIECESDATA.map(pieceData => {
+            return {
+              playerIndex: index,
+              playerId: id,
+              pieceClass: color,
+              pieceData: pieceData
+            }
+          }),
+        }
+      }),
+      selectedPiece: null,
     }
   }
 
-  componentDidMount(){
-  }
-
-  addPlayer(){
-    const player = {
-      pieces:pieces
-    }
+  selectPiece = (piece) => {
     this.setState({
-      players:[...this.state.players,player]
+      selectedPiece: piece
     })
   }
-  render(){
+
+  rotatePiece = (piece) => {
+    const rotation = math.pi / 2;
+    const C = math.cos(rotation).toFixed();
+    const S = math.sin(rotation).toFixed();
+    const originPiece = this.state.selectedPiece;
+    let rotatedPiece = Object.assign({},originPiece,{
+      pieceData: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+      ],
+    });
+    originPiece.pieceData.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        if (col) {
+          let [
+            [x],
+            [y],
+            // eslint-disable-next-line
+            [z]
+          ] = math.multiply([
+            [C, -S, (1 - C) * 2 + S * 2],
+            [S, C, (1 - C) * 2 - S * 2],
+            [0, 0, 1]
+          ], [
+            [rowIndex],
+            [colIndex],
+            [1]
+          ])
+          rotatedPiece.pieceData[x][y] = originPiece.playerId
+        }
+      })
+    })
+    this.setState({
+      selectedPiece: rotatedPiece
+    })
+  }
+
+  mirrorPiece = (piece) => {
+    const originPiece = this.state.selectedPiece;
+    let rotatedPiece = Object.assign({},originPiece,{
+      pieceData: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+      ],
+    });
+    originPiece.pieceData.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        if (col) {
+          let x = 4-rowIndex;
+          let y = colIndex;
+          rotatedPiece.pieceData[x][y] = originPiece.playerId
+        }
+      })
+    })
+    this.setState({
+      selectedPiece: rotatedPiece
+    })
+  }
+
+  render() {
     const players = this.state.players;
-    const playerItems = players.map((player,playerIndex)=>{
-      const pieces = player.pieces;
+    const selectedPiece = this.state.selectedPiece;
+    const boardMatrix = this.state.boardMatrix;
+    const playerItems = players.map(player => {
       return (
-        <PlayerItem pieces={pieces}></PlayerItem>
+        <PlayerItem player={player} selectPiece={this.selectPiece} key={player.id}></PlayerItem>
       )
     })
     return (
       <div className='square'>
         {playerItems}
-        <Board boardMatrix={this.state.boardMatrix}></Board>
+        {selectedPiece && <PieceItem piece={selectedPiece} isSelected={true} rotatePiece={this.rotatePiece} mirrorPiece={this.mirrorPiece}></PieceItem>}
+        <Board boardMatrix={boardMatrix}></Board>
       </div>
     )
   }
 }
-const pieces = [
+
+//棋子数据
+const PIECESDATA = [
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 1, 1, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [1, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 1, 0, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 1, 0],
-      [0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 1, 1, 0, 0],
-      [0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [1, 1, 1, 1, 1],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 1, 0, 0, 0],
-      [0, 1, 1, 1, 1],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 1, 1, 1, 1],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 1, 1],
-      [0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1],
+    [0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 1, 1, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 1, 0, 1, 0],
-      [0, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 1, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 1, 0],
-      [0, 0, 1, 0, 0],
-      [0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 1, 0, 0, 0],
-      [0, 1, 0, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0]
   ],
   [
-      [0, 0, 0, 0, 0],
-      [0, 1, 0, 0, 0],
-      [0, 1, 1, 0, 0],
-      [0, 0, 1, 1, 0],
-      [0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0]
   ]
 ]
 
